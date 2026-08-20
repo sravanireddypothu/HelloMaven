9ad8d84b4e98cb9bbaf93a4d2642101dd60ed732
sravanireddypothu <pothusravanireddy2013@gmail.com>
2026-08-20 23:25:49 +0530

    Hello World

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,7 +1,13 @@
 package com.example;
 
-public class App {
-    public static void main(String[] args) {
-        System.out.println("Hello World! CMRCET");
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
     }
 }
